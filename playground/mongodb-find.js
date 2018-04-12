120536e6e8bc377f30898f1cdb6c001e3c96d834
Fernanda Maia <nanda.najah@gmail.com>
2018-04-12 10:07:58 -0300

    Implementing find and delete files

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -8,23 +8,23 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,client) => {
     console.log('Connected to MongoDB server');
     const db = client.db('TodoApp');
    
-    // db.collection('Todos').find(
-    //     {
-    //         _id: new ObjectID('5ac660a0950f332308a24c6f')
-    //     }
-    // ).toArray().then((docs) => {
-    //     console.log('Todos');
-    //     console.log(JSON.stringify(docs, undefined,2));
-    // },  (err) => {
-    //     console.log('Unable to fetch data', err);
-    // });
+    db.collection('Todos').find(
+        {
+            _id: new ObjectID('5ac660a0950f332308a24c6f')
+        }
+    ).toArray().then((docs) => {
+        console.log('Todos');
+        console.log(JSON.stringify(docs, undefined,2));
+    },  (err) => {
+        console.log('Unable to fetch data', err);
+    });
     
-    // db.collection('Todos').find().count().then((count) => {
-    //     console.log(`Todos ${count}`);
-    //     console.log(JSON.stringify(docs, undefined,2));
-    // },  (err) => {
-    //     console.log('Unable to fetch data', err);
-    // });
+    db.collection('Todos').find().count().then((count) => {
+        console.log(`Todos ${count}`);
+        console.log(JSON.stringify(docs, undefined,2));
+    },  (err) => {
+        console.log('Unable to fetch data', err);
+    });
 
     db.collection('Users').find(
         {
