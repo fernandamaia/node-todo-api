e3f8eac06d3fea32a2f45111c47ca609545fd2e0
Fernanda Maia <nanda.najah@gmail.com>
2018-04-13 09:36:40 -0300

    Deploy to Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://fernanda:fernanda@ds161873.mlab.com:61873/node-todo-api');
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const {User} = require('./models/user');
 const {Todo} = require('./models/todo');
 
 var app = express();
+const port = process.env.PORT || 3000;  
+
 
 app.use(bodyParser.json());
 
@@ -50,7 +52,7 @@ app.get('/todos/:id', (req,res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
 
