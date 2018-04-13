69db3e8aad6596dbc7f7aba2bba04d213bec6f1f
Fernanda Maia <nanda.najah@gmail.com>
2018-04-13 09:50:31 -0300

    Tirando arquivo yarn.lock

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://fernanda:fernanda@ds161873.mlab.com:61873/node-todo-api');
+
+//mongoose.connect(process.env.MONGO-URI || 'mongodb://localhost:27017/TodoApp'); //com heroku add-on
+
+
+mongoose.connect('mongodb://fernanda:fernanda@ds161873.mlab.com:61873/node-todo-api'); //com mLab direto
 
 module.exports = {mongoose};
