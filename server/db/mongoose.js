5b7566788aaf62f41349f37fc22778f4250fbcd4
Fernanda Maia <nanda.najah@gmail.com>
2018-04-13 10:56:38 -0300

    Heroku url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/TodoApp'); //com heroku add-on
+//mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/TodoApp'); //com heroku add-on
 
 
-//mongoose.connect('mongodb://fernanda:fernanda@ds161873.mlab.com:61873/node-todo-api'); //com mLab direto
+mongoose.connect('mongodb://fernanda:fernanda@ds161873.mlab.com:61873/node-todo-api'); //com mLab direto
 
 module.exports = {mongoose};
