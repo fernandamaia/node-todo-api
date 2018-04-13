265685a5bbd7be7017a4f7d457f5422a58bf9566
Fernanda Maia <nanda.najah@gmail.com>
2018-04-13 14:00:24 -0300

    Testando configurações servidor

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,21 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
- try {
-     mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/TodoApp'); //com heroku add-on ou local
- } catch(e) {
-    mongoose.close();
-    mongoose.connect('mongodb://fernanda:fernanda@ds161873.mlab.com:61873/node-todo-api'); //mLab direto
- }
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/TodoApp'); //com heroku add-on ou local
+//mongoose.connect('mongodb://fernanda:fernanda@ds161873.mlab.com:61873/node-todo-api'); //mLab direto
 
-// mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/TodoApp', function(err) {
-//     if (err) {
-//         // console.log(err);
-//         // console.log("erro");
-//         // mongoose.connect('mongodb://fernanda:fernanda@ds161873.mlab.com:61873/node-todo-api')
-//     }
-// });
-
-//mongoose.connect('mongodb://fernanda:fernanda@ds161873.mlab.com:61873/node-todo-api'); //com mLab direto
 
 module.exports = {mongoose};
