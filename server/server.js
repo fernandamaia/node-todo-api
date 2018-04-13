a260cc0cbe2e135db969eeb268a302d1ec435b4b
Fernanda Maia <nanda.najah@gmail.com>
2018-04-13 11:35:42 -0300

    PATCH todos/:id added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+
 const {mongoose} = require('./db/mongoose.js') ;
 const {User} = require('./models/user');
 const {Todo} = require('./models/todo');
@@ -62,9 +64,36 @@ app.delete('/todos/:id', (req,res) => {
         }
         res.send({todo});
     }).catch((e) => {
-        res.status(404).send();
+        res.status(400).send();
     });
  })
+
+
+app.patch('/todos/:id', (req,res) => {
+    var id =  req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    var body = _.pick(req.body, ['text','completed']);
+    
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new:true}).then((todo) => {
+        if (!todo) {
+           res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
