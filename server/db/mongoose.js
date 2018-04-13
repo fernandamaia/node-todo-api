04493df75dbd8c3ba0bd2315e9f6c4c5a0990243
Fernanda Maia <nanda.najah@gmail.com>
2018-04-13 11:42:17 -0300

    Testando configuracao conexão banco

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-try {
-    mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/TodoApp'); //com heroku add-on ou local
-} catch(e) {
+// try {
+//     mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/TodoApp'); //com heroku add-on ou local
+// } catch(e) {
     mongoose.connect('mongodb://fernanda:fernanda@ds161873.mlab.com:61873/node-todo-api'); //mLab direto
-}
+// }
 
 
 
